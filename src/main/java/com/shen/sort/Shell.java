17f8bc2f01a16a3a952472bfd6325c4dfc075cc5
dimhunter <43543865@qq.com>
2016-07-21 17:57:24 +0800

    希尔排序完善

--- src/main/java/com/shen/sort/Shell.java
+++ src/main/java/com/shen/sort/Shell.java
@@ -18,6 +18,7 @@ public class Shell extends SortBase {
         }
         while(h >= 1){
             //将数组变为h有序
+            //将此处代码和插入排序的2个for循环比较，其实就是i++换成了j -= h，h增量的增量。
             for(int i = h; i<length; i++){
                 //将a[i]插入到a[i-h],a[i-2*h],a[i-3*h]... 之中
                 for(int j = i; j >= h && less(arr[j],arr[j-h]); j -= h){
@@ -30,7 +31,7 @@ public class Shell extends SortBase {
 
     public static void main(String[] args) {
         Shell shell = new Shell();
-        String[] arr = new String[]{"d","b","c","a","x","z","y","8","5","7","2","6","9","m","k","0"};
+        String[] arr = new String[]{"d","b","c","a","x","z","y","8","5","7","2","6","9","m","k","0","-38", "22", "-13"};
         shell.sort(arr);
         assert shell.isSorted(arr);
         shell.show(arr);
