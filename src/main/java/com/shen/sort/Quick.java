84ec5f84484a63331128bd552684386742b0c621
dimhunter <43543865@qq.com>
2016-10-05 09:51:56 +0800

    fdafsfdsaf

--- src/main/java/com/shen/sort/Quick.java
+++ src/main/java/com/shen/sort/Quick.java
@@ -1,46 +1,57 @@
 package com.shen.sort;
 
-import com.shen.SortBase;
 /**
  * 快速排序
+ * 
+ * 基本思想：选择一个基准元素,通常选择第一个元素或者最后一个元素,通过一趟扫描，将待排序列分成两部分,一部分比基准元素小,一部分大于等于基准元素,
+ * 此时基准元素在其排好序后的正确位置,
+ * 然后再用同样的方法递归地排序划分的两部分。
  * @author shen
  *
  */
-public class Quick extends SortBase{
+public class Quick{
 
-	@Override
-	public void sort(Comparable[] a) {
-		quickSort(a,0,a.length-1);
+	public void sort(int[] arr) {
+		quickSort(arr,0,arr.length-1);
 	}
 
-	private void quickSort(Comparable[] a, int left, int right) {
-		if(right <= left){
-			return ;
+	private void quickSort(int[] arr, int left, int right) {
+		//满足 left<right 条件才进行排序,
+		if(left < right){
+			//将数组进行一分为二
+			int middle = partition(arr,left,right);
+			System.out.println("---"+middle);
+			//左右2边分别递归调用quickSort进行排序
+			quickSort(arr,left,middle - 1);
+			quickSort(arr,middle + 1,right);
 		}
-		int j = partition(a,left,right);
-		quickSort(a,left,j-1);
-		quickSort(a,j+1,right);
 	}
 
-	private int partition(Comparable[] a, int left, int right) {
-		int i = left, j = right+1;
-		Comparable v = a[left];
-		while(true){
-			while(less(a[++i],v)) if(i == right) break;
-			while(less(v, a[--j])) if(j == left) break;
-			if(i>=j) break;
-			exch(a,i,j);
+	private int partition(int[] arr, int left, int right) {
+		//数组的第一个元素作为基准，提到while循环外边，最后在赋值用，不会导致丢值。
+		int tmp = arr[left];
+		while(left < right){
+			//找到比基准元素小的元素位置
+			while(left < right && arr[right] >= tmp){
+				right -- ;
+			}
+            arr[left] = arr[right]; 
+            while(left < right && arr[left]<=tmp){
+                left ++;
+            }
+            arr[right] = arr[left];
 		}
-		exch(a,left,j);
-		return j;
+		arr[left] = tmp;
+		return left;
 	}
 	
 	public static void main(String[] args) {
 		Quick q = new Quick();
-		String[] arr = new String[]{"Q","U","I","C","K","S","O","R","T","E","X","A","M","P","L","E"};
+		int[] arr ={49,38,65,97,76,13,27,49,78,34,12,64,1,8};
         q.sort(arr);
-        assert q.isSorted(arr);
-        q.show(arr);
+        for(int i=0; i<arr.length; i++ ){
+            System.out.print(arr[i] + " ");
+        }
 	}
 
 }
--- src/main/java/com/shen/sort/Shell.java
+++ src/main/java/com/shen/sort/Shell.java
@@ -3,6 +3,8 @@ package com.shen.sort;
 import com.shen.SortBase;
 
 /**
+ * 希尔排序就是在插入排序的基础上加入一个外循环，将h按照递增序列递减。
+ * 适用于大型数组。解决的是插入排序只会交换相邻元素的弊端，跨大步的交换。比如最小值在数组末尾，这样交换就会节省时间。
  * Created by shenge on 16/7/6.
  */
 public class Shell extends SortBase {
@@ -11,11 +13,13 @@ public class Shell extends SortBase {
     public void sort(Comparable[] arr) {
         int length = arr.length;
         int h = 1;
-        //通过3*h + 1计算出循环的因子。
+        //1. 通过3*h + 1计算出循环的因子。
         while(h < length/3){
             //1,4,13,40,121,.....
             h = 3*h + 1;
         }
+        System.out.println("h="+h+",length="+length);
+        //2. 将计算出的循环因子h加入到插入排序的外循环，h=h/3进行递减。
         while(h >= 1){
             //将数组变为h有序
             //将此处代码和插入排序的2个for循环比较，其实就是i++换成了j -= h，h增量的增量。
